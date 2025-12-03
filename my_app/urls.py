81bd7b73ca6f53764bd5e7a2e8538ec14985de9f
nourbatniji <nour.albattniji@gmail.com>
2025-12-03 15:31:29 +0200

    Add templates, static files, models, views

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,11 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('login', views.login),
-    path('signup', views.signup),
-    path('dashboard', views.dashboard),
-    path('classrooms', views.classrooms_page),
-    path('challenges', views.challenges_page),
-    path('leaderboard', views.leaderboard_page),
-    path('profile', views.profile_page),
+    path('login/', views.login),
+    path('signup/', views.signup),
+    path('dashboard/', views.dashboard),
+    path('classrooms/', views.classrooms_page),
+    path('classroom/<int:classroom_id>/', views.classroom_detail),
+    path('challenges/', views.challenges_page),
+    path('challenge/<int:challenge_id>/', views.challenge_detail),
+    path('leaderboard/', views.leaderboard_page),
+    path('profile/', views.profile_page),
+    path('mentor-dashboard/', views.mentor_dashboard),
 ]
+
--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import models
+import bcrypt
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'login.html')
-
+#  SIGNUP 
 def signup(request):
+    if request.method == 'POST':
+        errors = models.validate_signup(request.POST)
+        if errors:
+            return render(request, 'signup.html', {'errors': errors})
+
+        user = models.create_user(request.POST)
+        request.session['user_id'] = user.id
+        request.session['is_logged'] = True
+        return redirect('/dashboard')
+
     return render(request, 'signup.html')
 
+
+#  LOGIN 
+def login(request):
+    if request.method == 'POST':
+        errors = models.validate_login(request.POST)
+        user = models.get_user_by_email(request.POST['email'])
+
+        if errors:
+            request.session['is_logged'] = False
+            return render(request, 'login.html', {'errors': errors})
+
+        if user:
+            logged_user = user[0]
+            if bcrypt.checkpw(request.POST['password'].encode(), logged_user.password.encode()):
+                request.session['user_id'] = logged_user.id
+                request.session['is_logged'] = True
+                return redirect('/dashboard')
+            else:
+                request.session['is_logged'] = False
+                errors['incorrect_pw'] = 'Incorrect Password'
+                return render(request, 'login.html', {'errors': errors})
+
+
+        return render(request, 'login.html', {'errors': errors})
+
+
+    return render(request, 'login.html')
+
+
+def signout(request):
+    request.session.flush()
+    return redirect('/')
 
 def dashboard(request):
     return render(request, 'dashboard.html')
@@ -17,11 +58,20 @@ def dashboard(request):
 def classrooms_page(request):
     return render(request, 'classrooms.html')
 
+def classroom_detail(request, id):
+    return render(request, 'classroom_details.html')
+
 def challenges_page(request):
     return render(request, 'challenges.html')
+
+def challenge_detail(request):
+    return render(request, 'challenge_details.html')
 
 def leaderboard_page(request):
     return render(request, 'leaderboard.html')
 
 def profile_page(request):
     return render(request, 'profile.html')
+
+def mentor_dashboard(request):
+    return render(request, 'mentor.html')
